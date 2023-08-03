935295b6c916d9b5387fb90a8a1faf215c281d60
Yeon09-a <kimyeona0926@gmail.com>
2023-08-03 17:20:08 +0900

    2023.08.03 아이템 기본 정보 추가 및 아이템 세부 정보 창 코드 작성 - Yeon09-a
    - 아이템 기본 정보 추가(폭탄, 물약, 주화, 작물, 씨앗, 창, 침낭)
    - 아이템 세부 정보 창(ItemInfoPanel) 코드 작성

--- SavingTheEarth/Assets/02. Scripts/Item/ItemIcon.cs
+++ SavingTheEarth/Assets/02. Scripts/Item/ItemIcon.cs
@@ -1,8 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ItemIcon : MonoBehaviour
+public class ItemIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public Item itemInfo;
+    public Item itemInfo; // 아이템 정보
+
+    private Transform itemInfoTr; // 아이템 정보 transform
+    private Transform canvasTr; // Canvas transform
+
+    private float curTime = 2.0f; // 마우스가 머무르는 시간
+
+    private bool isOpened = false; // 2초 타이머 시작
+    private void Start()
+    {
+        canvasTr = GameObject.FindWithTag("Canvas").transform;
+        itemInfoTr = canvasTr.GetChild(canvasTr.childCount - 1).transform;
+    }
+
+    private void Update()
+    {
+        if (isOpened) // 타이머 시작
+        {
+            curTime -= Time.deltaTime;
+            if(curTime < 0) // 2초 지나면
+            {
+                // 판넬 활성화
+                itemInfoTr.GetChild(1).GetComponent<RawImage>().texture = itemInfo.image;
+                itemInfoTr.GetChild(2).GetComponent<TextMeshProUGUI>().text = itemInfo.itName;
+                itemInfoTr.GetChild(3).GetComponent<TextMeshProUGUI>().text = itemInfo.info;
+                itemInfoTr.GetComponent<RectTransform>().position = transform.GetChild(0).position + new Vector3(-250, 100, 0);
+                itemInfoTr.gameObject.SetActive(true);
+                curTime = 2.0f;
+                isOpened = false; // 타이머가 끝났으므로 false
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isOpened = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isOpened = false;
+        itemInfoTr.gameObject.SetActive(false);
+    }
 }
