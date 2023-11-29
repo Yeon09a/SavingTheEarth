af13f9bff0667be296f9b6717a6b3507c00de745
mxyffff <rua0254@naver.com>
2023-11-29 15:22:17 +0900

    초상화 & 대화 추가 (미완)
    독백처리 및 오브젝트 느낌표 처리 필요합니다(하는 중)

--- SavingTheEarth/Assets/02. Scripts/DialogSystem/ConversationManager.cs
+++ SavingTheEarth/Assets/02. Scripts/DialogSystem/ConversationManager.cs
@@ -27,10 +27,42 @@ public class ConversationManager : MonoBehaviour
     // 대사 & 초상화 생성 함수
     void GenerateData()
     {
-        dialogData.Add(100, new string[] { "이동: WASD 또는 방향키\n달리기: shift\n아이템 상호작용: e 또는 마우스\n인벤토리 선택: 숫자 1 ~ 5" });
-        dialogData.Add(200, new string[] { "1. 온실로 가서 물 주기\n2. 교수님과의 통신 확인하기" });
+        dialogData.Add(100, new string[] {  // 조종대
+            "이동: WASD 또는 방향키\n달리기: shift\n아이템 상호작용: e 또는 마우스\n인벤토리 선택: 숫자 1 ~ 5/6",
+            "오늘도 아무런 연락이 없으시네..\n교수님은 대체 언제 오시는거지?/4", 
+            "설마 진짜로 무슨 일이라도 생기신 거 아니야?/3", "나.. 졸업은 할 수 있을까.../1",
+            "진짜 직접 찾으러 가봐야 하나...(언짢)/5",
+            "에휴 졸업하려면 어쩔 수 없지\n일단 교수님 통신이 어디서부터 끊겼는지 확인해봐야겠어/2"
+        });
+        dialogData.Add(200, new string[] { // 칠판
+            "1. 온실로 가서 물 주기\n2. 교수님과의 통신 확인하기/6",
+            "일단 온실에 가서 물을 먼저 줄까?/0"
+        });
+        dialogData.Add(300, new string[] { // 온실 문
+            "이제 조종실로 가서 통신을 확인해보자/0"
+        });
 
-        // portraitData.Add(~~~(id) + n, portraitArr[n]); // 후에 수정
+        //portraitData.Add(100 + 0, portraitArr[0]); // normal face
+        //portraitData.Add(100 + 1, portraitArr[1]); // closed eye
+        //portraitData.Add(100 + 2, portraitArr[2]); // stare face
+        //portraitData.Add(100 + 3, portraitArr[3]); // side eye
+        //portraitData.Add(100 + 4, portraitArr[4]); // little annoying
+        //portraitData.Add(100 + 5, portraitArr[5]); // very annoying
+
+        //portraitData.Add(200 + 0, portraitArr[0]); // normal face
+        //portraitData.Add(200 + 1, portraitArr[1]); // closed eye
+        //portraitData.Add(200 + 2, portraitArr[2]); // stare face
+        //portraitData.Add(200 + 3, portraitArr[3]); // side eye
+        //portraitData.Add(200 + 4, portraitArr[4]); // little annoying
+        //portraitData.Add(200 + 5, portraitArr[5]); // very annoying
+
+        portraitData.Add(0, portraitArr[0]); // normal face
+        portraitData.Add(1, portraitArr[1]); // closed eye
+        portraitData.Add(2, portraitArr[2]); // stare face
+        portraitData.Add(3, portraitArr[3]); // side eye
+        portraitData.Add(4, portraitArr[4]); // little annoying
+        portraitData.Add(5, portraitArr[5]); // very annoying
+        portraitData.Add(6, portraitArr[6]); // 보여지지 않을 초상화
     }
 
     // 대화 반환 함수
@@ -42,8 +74,12 @@ public class ConversationManager : MonoBehaviour
             return dialogData[id][dialogIndex]; // 대사 반환
     }
 
-    public Sprite GetPortrait(int id, int portraitIndex)
+    //public Sprite GetPortrait(int id, int portraitIndex)
+    //{
+    //    return portraitData[id + portraitIndex]; // 초상화 이미지 반환
+    //}
+    public Sprite GetPortrait(int portraitIndex) // 초상화 이미지 리턴 (portraitIndex == 초상화 번호)
     {
-        return portraitData[id + portraitIndex]; // 초상화 이미지 반환
+        return portraitData[portraitIndex]; // 초상화 이미지 반환
     }
 }
