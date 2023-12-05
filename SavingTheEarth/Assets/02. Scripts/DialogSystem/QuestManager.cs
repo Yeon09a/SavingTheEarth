cb180be011f66c5ee3eaea8f85f2998998b22b13
Yeon09-a <kimyeona0926@gmail.com>
2023-12-06 03:39:40 +0900

    2023.12.06 퀘스트 변경 - Yeon09-a

--- SavingTheEarth/Assets/02. Scripts/DialogSystem/QuestManager.cs
+++ SavingTheEarth/Assets/02. Scripts/DialogSystem/QuestManager.cs
@@ -80,10 +80,12 @@ public class QuestManager : MonoBehaviour
                     questObject[1].SetActive(true);
                 break;
             case 40:
+
+                convoManager.dialogData[1000] = new string[] {
+            "1. 식량 -> 온실\n2. 무기 -> 조종실 책상\n3. 침낭 -> 내 방\n4. 미니 잠수함 키 -> 교수님 방 책상/6"};
+
                 if (questActionIndex == 1)
                 {
-                    convoManager.dialogData.Add(1000, new string[] {
-            "1. 식량 -> 온실\n2. 무기 -> 조종실 책상\n3. 침낭 -> 내 방\n4. 미니 잠수함 키 -> 교수님 방 책상/6"});
                     questObject[1].SetActive(false);
                     questObject[2].SetActive(true);
                 }
